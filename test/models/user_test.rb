584b5fa12b87d63d7da971f512cc6604e346f7d1
Tarmo Petrovits <tarmo88@gmail.com>
2023-07-21 13:58:31 +0300

    Make a basic User model ...
    ... (including secure passwords)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "test@example.com")
+    @user = User.new(name: "Example User", email: "test@example.com",
+    password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -51,7 +52,16 @@ class UserTest < ActiveSupport::TestCase
   test "email addresses should be unique" do
     duplicate_user = @user.dup
     @user.save
-    duplicate_user.email = @user.email.upcase
     assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
